c07392444e54f3f5e080e0bd7c9c290812135b70
inthewalter <21400564@handong.edu>
2018-08-23 05:21:06 +0900

    1번 방식으로 일단 데이터 동기화 문제 해결

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,7 +49,7 @@ export const firebaseConfig = {
     BrowserModule,
     IonicModule.forRoot(MyApp, {
       menuType: 'overlay',
-      mode: 'md',
+      mode: 'md', // scss를 모두 안드로이드로 통일해봤음
       pageTransition: 'md-transition', // 안드로이드 화면전환이 렉이 덜 걸리는 듯
       // backButtonIcon: 'md-arrow-back',
       // backButtonText: '',
