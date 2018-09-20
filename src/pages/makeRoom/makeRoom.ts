251d915f11f251297f0c9572fa09fc381793f06a
HyoeunChoi <21000763@handong.edu>
2018-09-20 11:15:39 +0900

    장흥초, 세차장 리스트 추가

--- src/pages/makeRoom/makeRoom.ts
+++ src/pages/makeRoom/makeRoom.ts
@@ -36,7 +36,7 @@ export class MakeRoomPage {
   userID: string;
   transportType: string;
 
-  spotList: Array<string> = ["한동대학교", "포항역", "양덕", "고속버스터미널", "시외버스터미널", "E1", "그랜드할인마트", "커피유야", "하나로", "북부해수욕장", "육거리", "직접입력"];;
+  spotList: Array<string> = ["한동대학교", "포항역", "양덕", "고속버스터미널", "시외버스터미널", "E1", "그랜드할인마트", "장흥초등학교", "세차장" ,"커피유야", "하나로", "북부해수욕장", "육거리", "직접입력"];;
 
   constructor(public alertCtrl: AlertController, public navParams: NavParams, public dateServices: DateProvider,
                public navCtrl:NavController, public af: AngularFireDatabase, public userServices: UsersProvider){
