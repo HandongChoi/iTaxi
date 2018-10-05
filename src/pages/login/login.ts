cff47fb5f3efe18065baeafc566dedf0fd497e25
inthewalter <21400564@handong.edu>
2018-10-05 14:25:32 +0900

    한글아이디 유혜영 추가

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -77,6 +77,10 @@ export class LoginPage {
       this.af.object(`/userProfile/21500351`, { preserveSnapshot: true }).subscribe( (data) => {
         this.authProvider.loginUser('21500351').then( ()=> this.loading.dismiss());        
       });
+    }else if(id == "유혜영" && password == "0764"){
+      this.af.object(`/userProfile/21500439`, { preserveSnapshot: true }).subscribe( (data) => {
+        this.authProvider.loginUser('21500439').then( ()=> this.loading.dismiss());        
+      });
     }else{
       //헤더 부분에다가 utf-8 적용해서 한글도 보낼 수 있도록 해야된다.
       this.http.post('https://8slpzkf3j9.execute-api.us-east-2.amazonaws.com/iTaxi/login',JSON.stringify(login))
