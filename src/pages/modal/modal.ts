8a2e0aa2fa55c26729109d3fec2afc4f3ccd822a
adonaiOhesed <adonaiOhesed@gmail.com>
2019-02-20 17:57:55 +0900

    리스트에서 방 들어갈대 캐리어 3개 초과 로직 기존 방꺼 고려하기로 수정

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -13,10 +13,10 @@ export class ModalPage {
   departTime: String = "";
   depart: String = "";
   arrive: String = "";
-  currentCarrierS: number = 0;
-  currentCarrierL: number = 0;
-  carrierS: number = 0;
-  carrierL: number = 0;
+  roomCarrierS: number = 0;
+  roomCarrierL: number = 0;
+  selectedCarrierS: number = 0;
+  selectedCarrierL: number = 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl : ViewController, public alertCtrl: AlertController) {
   }
@@ -27,12 +27,13 @@ export class ModalPage {
     this.departTime = this.room['departTime'];
     this.depart = this.room['depart'];
     this.arrive = this.room['arrive'];
-    this.currentCarrierS = this.room['carrierS'];
-    this.currentCarrierL = this.room['carrierL'];
+    this.roomCarrierS = this.room['carrierS'];
+    this.roomCarrierL = this.room['carrierL'];
   }
   
+  //modal이 닫힐때 data는 밑에 방식으로 넘겨줘서 list에서 받게 된다.
   closeModal(){    
-    let data = {carrierS: this.carrierS, carrierL: this.carrierL};
+    let data = {carrierS: this.selectedCarrierS, carrierL: this.selectedCarrierL};
     this.viewCtrl.dismiss(data);
   }
   dismiss(){
@@ -40,10 +41,10 @@ export class ModalPage {
   }
 
   carrierCapacityCheck(carrier, carrierSize){
-    var carrierCapacity = 3;
-    var sumOfCarrier = carrierSize == 'S' ?  carrier + this.carrierL : this.carrierS + carrier; 
+    var carrierCapacity = 3 - (this.roomCarrierS + this.roomCarrierL);
+    var sumOfCarrier = carrierSize == 'S' ?  carrier + this.selectedCarrierL : this.selectedCarrierS + carrier;
     if(sumOfCarrier <= carrierCapacity){
-      carrierSize == 'S' ? this.carrierS = carrier : this.carrierL = carrier;
+      carrierSize == 'S' ? this.selectedCarrierS = carrier : this.selectedCarrierL = carrier;
     }else{
       let alert = this.alertCtrl.create({
         message: "트렁크에 캐리어는 최대 3개만 실을 수 있어요 T.T",
